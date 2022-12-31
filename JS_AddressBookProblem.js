91e94ab7e7af6f3774b509cdb53db810de43e79a
Piyushnmj <piyushnmj@yahoo.com>
2022-12-31 20:18:19 +0530

    Count of contacts in address book

--- JS_AddressBookProblem.js
+++ JS_AddressBookProblem.js
@@ -158,3 +158,13 @@ function Modify()
             break;
     }
 }
+
+//Usecase 6: Ability to find number of contacts in the address book
+
+function Findcoint(count)
+{
+    return count+1;
+}
+
+let totalCount=contactList.reduce(Findcoint,0);
+console.log("Total number of contacts in AddressBook: "+totalCount);
